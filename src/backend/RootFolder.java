c4117af65d2c62a5116b886cfe4629be687dc3dd
hirvisuo <71279400+hirvisuo@users.noreply.github.com>
2020-09-22 10:32:01 +0300

    Updated RootFolder Class

--- src/backend/RootFolder.java
+++ src/backend/RootFolder.java
@@ -5,55 +5,21 @@ import java.util.List;
 
 import backend.absClasses.FolderABS;
 
-public class RootFolder extends FolderABS{
+public class RootFolder extends FolderABS {
+	
 	public RootFolder() {
-		super("All Sound Clips", null, new ArrayList<Album>(), new ArrayList<FolderABS>());
-	}
-
-	/*
-	 * Gets all the Albums that have been created
-	 * 
-	 * @return List with all the albums
-	 */
-	@Override
-	public List<Album> getAlbums() {
-		List<FolderABS> allSubFolders = getAllChildren();
-		List<Album> allAlbums = new ArrayList<Album>();
-		
-		for(FolderABS subFolder: allSubFolders) {
-			for(Album subAlbum: subFolder.getAlbums()) {
-				allAlbums.add(subAlbum);
-			}
-		}
-		return allAlbums;
+		super("All Sound Clips", null, new ArrayList<FolderABS>(), new ArrayList<SoundClip>());
 	}
 	
-	/*
-	 * Changes the parent. As this is the root folder this action is not allowed
-	 * 
-	 * @return false
-	 */
-	@Override
-	public boolean changeParent(FolderABS newParent) {
-		return false;
-	}
-	/*
-	 * Deletes this folder. As this is the root folder we can't do it
-	 * 
-	 * @return false
-	 */
 	@Override
 	public boolean delete() {
 		return false;
 	}
-
-	/*
-	 * Gets this instance for sub folders to use
-	 * 
-	 * @return this
-	 */
+	
 	@Override
 	public FolderABS getRoot() {
 		return this;
 	}
+	
+
 }
